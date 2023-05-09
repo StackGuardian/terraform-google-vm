444330a831fa107f536ec66b5149c93b52cc1e6e
Johannes Scheuerer <joscheuerer@gmail.com>
2023-05-09 21:56:30 +0200

    Update main.tf

--- modules/instance_template/main.tf
+++ modules/instance_template/main.tf
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
+/*
 #########
 # Locals
 #########
@@ -189,3 +189,5 @@ resource "google_compute_instance_template" "tpl" {
     }
   }
 }
+  
+*/
